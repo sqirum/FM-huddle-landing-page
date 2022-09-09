cd8154c334d7014a737dfd536dbb41a5c1eb3a89
sqirum <pauloslh94@gmail.com>
2022-09-10 00:51:13 +0100

    finish exercise

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,20 @@ module.exports = {
         body: 'Poppins'
       },
       maxWidth: {
-        laptop: '1440px'
+        laptop: '1440px',
+        logo: '200px'
       },
       backgroundImage: {
-        mobile: 'url("images/bg-mobile.svg")',
-        desktop: 'url("images/bg-desktop.svg")'
+        mobile: 'url("/images/bg-mobile.svg")',
+        desktop: 'url("/images/bg-desktop.svg")'
+      },
+      backgroundColor: {
+        purple: '#684BB1',
+        pink: '#E880E8'
+      },
+      textColor: {
+        purple: '#684BB1',
+        pink: '#E880E8'
       }
     },
   },
